bbe958e23059142bad25752129c7064a7ad905fc
vikasmaliya004 <vikasmaliya004@gmail.com>
2022-06-12 09:18:15 +0530

    complete project

---
+++ word.js
@@ -0,0 +1,15 @@
+let textbox = document.getElementById("textbox");
+textbox.addEventListener('input', function() {
+    var text = this.value;
+    // console.log(text);
+    let char = text.length;
+    document.getElementById("char").innerHTML = char;
+
+    text = text.trim();
+    let words = text.split(" ");
+
+    let cleanList = words.filter(function(elm) {
+        return elm != "";
+    });
+    document.getElementById("word").innerHTML = cleanList.length;
+})
